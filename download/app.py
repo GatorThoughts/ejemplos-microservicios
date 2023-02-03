fed08bb3739776c9429156f1d1044e8512f746d9
GatorThoughts <jmdeleonpi@gmail.com>
2023-02-03 12:17:34 -0600

    Habilitar ruta / para Health check

--- download/app.py
+++ download/app.py
@@ -7,9 +7,22 @@ app = Flask(__name__)
 
 
 @app.route('/<hash>/download', methods=['GET'])
-def upload_file(hash):
+def download_file(hash):
     #Descargar el archivo desde el folder compartido
     return send_file(f'{IMG_FOLDER}/{hash}.png', mimetype='png', as_attachment=True)
+
+@app.route('/', methods=['GET'])
+def test_file():
+    #Descargar el archivo desde el folder compartido
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=image>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=8000)
